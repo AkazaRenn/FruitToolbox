9885abee365a107dc11aa3f076c8df48c390a3e2
Renn <8340896+AkazaRenn@users.noreply.github.com>
2022-09-20 17:08:32 -0400

    Fix wrong namespace

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,7 +21,7 @@ using FruitLanguageSwitcher.Interlop;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace LanguageSwitcher
+namespace FruitLanguageSwitcher
 {
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
