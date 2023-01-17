a37fa9ae37ab80289e0ef434c472894d6dff0dda
Renn <8340896+AkazaRenn@users.noreply.github.com>
2023-01-16 23:47:50 -0500

    Extend wait time for slow PC

--- Core/Hotkey.cs
+++ Core/Hotkey.cs
@@ -10,7 +10,7 @@ namespace FruitLanguageSwitcher.Core
         public const int onCapsLockMessage = 1;
         public const int onLanguageChangeMessage = 2;
                     
-        public const int windowActivateWaitMs = 200;
+        public const int windowActivateWaitMs = 500;
 
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
         private delegate void AHKDelegate(int s);
